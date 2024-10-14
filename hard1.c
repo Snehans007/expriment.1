ffd6def0ac402a6ec53c8c27d3c3ae3ee16ef59e
Snehans007 <mssnehan2006@gmail.com>
2024-10-14 20:10:33 +0530

    Add files via upload

---
+++ hard1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+    int a;
+    scanf("%d",&a);
+    float b;
+    b=3*sqrt(25+10*sqrt(5))*pow(a,2);
+    printf("%f",b);
+    return 0;
+}
+
---
+++ mediam 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b,c,sum;
+    scanf("%d %d %d",&a,&b,&c);
+    sum=a+b+c;
+    printf("%d \n",sum);
+    float d;
+    d=sum/3.0;
+    printf("%f\n",d);
+    return 0;
+}
